d8a4be4c7fc98809ec1e9bbd3c4dc079191d9d19
Oleksandr Bron <lexlutermotion@gmail.com>
2023-06-04 21:41:29 +0300

    bug fixing

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/delivery-app">
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/delivery-app">
+        <React.StrictMode>
           <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+        </React.StrictMode>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
