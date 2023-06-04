3ef3598dba8f0f16ea7c59099c27297092e32969
Oleksandr Bron <lexlutermotion@gmail.com>
2023-06-04 14:26:29 +0300

    client side completed

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -1,2 +1,16 @@
-import { useSelector } from "react-redux";
+import Order from "../Order/Order";
 import css from "./OrderList.module.css";
+
+export default function OrderList({ order }) {
+  return (
+    <>
+      <ul className={css.orderList}>
+        {order.map((el) => (
+          <li className={css.orderListItem} key={"cart-" + el.id}>
+            <Order item={el} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
